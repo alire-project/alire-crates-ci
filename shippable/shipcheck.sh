0d9237175538c123c65732b1ba2010d858166ca2
Shippable Build <build@shippable.com>
2019-05-16 15:42:31 +0000

    alire@68c72635 [skip ci]

--- shippable/shipcheck.sh
+++ shippable/shipcheck.sh
@@ -6,37 +6,16 @@ trap 'echo "Interrupted" >&2 ; exit 1' INT
 set -o errexit
 set -o nounset
 
-# This script is triggered after a commit/PR to alire or alire-index
-# It checks that the trigger is not a PR but a real commit
-# This can probably be done through shippable resources,
-#   although I'm unsure if the added complexity is worth it.
-
-# Get alire
-git clone https://github.com/alire-project/alire
-pushd alire
-git checkout $BRANCH
-commit=`git log --pretty=oneline -1 | cut -c1-8`
-popd
-
-# Check changed commit
-touch shippable/$BRANCH.txt
-oldcommit=`cat shippable/$BRANCH.txt`
-
-if [ "$commit" == "$oldcommit" ]; then
-    echo Commit unchanged, stopping now.
-    exit 0
-else
-    echo "Testing crates because of alire@$BRANCH changes:"
-    echo "Old commit: $oldcommit"
-    echo "New commit: $commit"
-fi
+# This script is triggered after a commit to alire or alire-index
+# Checks in shippable.yml should ensure that it only runs after
+#   a real commit (not PR) to master or stable
 
 # Force testing by committing to ourselves
 # We do this instead of running shiptest.sh because this way unit tests are managed by shippable automatically
 git config --global user.email "shippable@alire-crates-ci"
 git config --global user.name "Shippable"
 git pull
-date -u > last-commit
-git add last-commit
+date -u > shippable/last-run
+git add shippable/last-run
 git commit -m "alire@$BRANCH triggered"
 git push git@github.com:alire-project/alire-crates-ci.git
