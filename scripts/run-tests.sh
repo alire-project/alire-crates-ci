91df695da32bb70ba0f6b91ee70c91da52081134
Jesper Quorning <jesper.quorning@gmail.com>
2023-08-30 17:41:33 +0200

    Install python modules with apt

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -7,12 +7,19 @@ set -o errexit
 case $DISTRO in
     arch-rolling)
         $sudo pacman -Sy
+        $sudo pip3 install -r requirements.txt
         ;;
-    debian-stable | ubuntu-lts | ubuntu-latest)
+    debian-stable)
         $sudo apt-get update
+        $sudo apt-get install $(awk 'print "python-" $0' requirements.txt)
+        ;;
+    ubuntu-lts | ubuntu-latest)
+        $sudo apt-get update
+        $sudo pip3 install -r requirements.txt
         ;;
     fedora-latest)
         $sudo yum makecache
+        $sudo pip3 install -r requirements.txt
         ;;
     *)
         echo WARNING: unsupported distro $DISTRO, not updating package metadata
@@ -21,8 +28,6 @@ case $DISTRO in
         # Maybe with alr exec -- pacman it will work.
         ;;
 esac
-
-$sudo pip3 install -r requirements.txt
 
 # Disable check for ownership that sometimes confuses docker-run git
 # Also, Github is not vulnerable to iCVE-2022-24765/CVE-2022-24767, see
