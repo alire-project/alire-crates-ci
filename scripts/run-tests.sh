0100b6bf1a8e37125e18fc407e1823adc4840055
Alejandro R. Mosteo <amosteo@unizar.es>
2026-01-21 12:48:17 +0100

    Check that there's no GLIB dependencies missing or skip

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 [ "$DISTRO" != "windows-latest" ] && sudo=sudo
 
+# Un-alias sudo if already running as root, to avoid issues with environments without sudo:
+[ "$(id -u)" -eq 0 ] && sudo=""
+
 case $DISTRO in
     arch-rolling)
         $sudo pacman -Sy
@@ -34,6 +37,21 @@ pip3 install -r requirements.txt
 # Also, Github is not vulnerable to iCVE-2022-24765/CVE-2022-24767, see
 # https://github.blog/2022-04-12-git-security-vulnerability-announced/
 git config --global --add safe.directory '*'
+
+# Check if gprbuild has GLIBC dependency issues. We need to do it in a temporary crate
+
+alr -n -f init --bin tempcrate
+pushd tempcrate
+
+if alr exec -- gprbuild --version 2>&1 | grep 'not found' | grep version | grep -q GLIBC; then
+    echo "gprbuild dependencies not found for the testing configuration."
+    echo "SKIPPING TEST BECAUSE OF gprbuild GLIBC DEPENDENCIES"
+    exit 0
+else
+    echo gprbuild ldd dependencies OK
+    popd
+    rm -rf tempcrate
+fi
 
 python3 -u -X utf8 test_release.py
 # -u: unbuffered output, simplifies GHA log interpretation on error
